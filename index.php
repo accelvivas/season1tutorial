b418885990f6e2ea07afc256e891fef79d5e49e8
Gian Accel Hernandez <accelvivas@gmail.com>
2025-08-14 14:34:05 +0800

    upd1

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$name = $address = $email = $section = $contact = "";
-$nameErr = $addressErr = $emailErr = $sectionErr = $contactErr = "";
+
+include('connections.php');
+
+$name = $address = $email = $section = $contact = $password = $cpassword = "";
+$nameErr = $addressErr = $emailErr = $sectionErr = $contactErr = $passwordErr = $cpasswordErr  = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["name"])) {
@@ -32,8 +35,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $contact = $_POST["contact"];
     }
+
+    if (empty($_POST["password"])) {
+        $passwordErr = "Password is required!";
+    } else {
+        $password = $_POST["password"];
+    }
+
+    if (empty($_POST["cpassword"])) {
+        $cpasswordErr = "Confirm Password is required!";
+    } else {
+        $cpassword = $_POST["cpassword"];
+    }
+
+ 
+    if ($name && $address && $email && $section && $contact && $password && $cpassword) {
+       
+        
+        $email_safe = mysqli_real_escape_string($connection, $email);
+
+        $check_email = mysqli_query($connection, "SELECT * FROM mytbl WHERE email = '$email_safe'");
+        $check_email_row = mysqli_num_rows($check_email);
+
+        if ($check_email_row > 0) {
+            $emailErr = "Email is already registered!";
+        } else {
+            $query = mysqli_query($connection, "INSERT INTO mytbl (name, address, email, section, contact, password, account_type) VALUES ('$name', '$address', '$email', '$section', '$contact', '$cpassword', '2')");
+       echo "<script language='javascript'>alert('New Record has been inserted!');</script>";
+       echo "<script>window.location.href='index.php';</script>";
+        }
+    }
 }
 ?>
+
+
 
 <style>
     .error{
@@ -54,20 +89,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-    <input type="text" name="name" value="<?php echo $name; ?>"> <br>
+Name:   <input type="text" name="name" value="<?php echo $name; ?>"> <br>
     <span class="error"><?php echo $nameErr; ?></span> <br>
 
-    <input type="text" name="address" value="<?php echo $address; ?>"> <br>
+Address:   <input type="text" name="address" value="<?php echo $address; ?>"> <br>
     <span class="error"><?php echo $addressErr; ?></span> <br>
 
-    <input type="text" name="email" value="<?php echo $email; ?>"> <br>
+Email:   <input type="text" name="email" value="<?php echo $email; ?>"> <br>
     <span class="error"><?php echo $emailErr; ?></span> <br>
 
-    <input type="text" name="section" value="<?php echo $section; ?>"> <br>
+Section:   <input type="text" name="section" value="<?php echo $section; ?>"> <br>
     <span class="error"><?php echo $sectionErr; ?></span> <br>
 
-    <input type="text" name="contact" value="<?php echo $contact; ?>"> <br>
+Contact:   <input type="text" name="contact" value="<?php echo $contact; ?>"> <br>
     <span class="error"><?php echo $contactErr; ?></span> <br>
+
+Password:   <input type="password" name="password" value="<?php echo $password; ?>"> <br>
+    <span class="error"><?php echo $passwordErr; ?></span> <br>
+
+Confirm Password:   <input type="password" name="cpassword" value="<?php echo $cpassword; ?>"> <br>
+    <span class="error"><?php echo $cpasswordErr; ?></span> <br>
 
     <input type="submit" value="submit">
 
@@ -76,16 +117,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <hr>
 <?php
 
-include('connections.php');
 
-if ($name && $address && $email && $section && $contact) {
-    $query = mysqli_query($connection, "INSERT INTO mytbl (name, address, email, section, contact) VALUES ('$name', '$address', '$email', '$section', '$contact')");
 
-    echo "<script>
-        alert('New Record has been inserted!');
-        window.location.href = 'index.php';
-    </script>";
-}
 
 
 $view_query = mysqli_query($connection, "SELECT * FROM mytbl");
